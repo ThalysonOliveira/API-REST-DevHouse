1aed9784417ab790d3cf12debad318b533d8764d
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-09-29 16:55:56 -0300

    'adicionadocamimhodasimagens'

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import route from 'color-convert/route'
 import express from 'express'
 import routes from './routes'
 import mongoose from 'mongoose'
+import path from 'path'
 
 class App {
     constructor() {
@@ -15,6 +16,7 @@ class App {
     }
 
     middlewares() {
+        this.server.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
         this.server.use(express.json())
     }
     routes() {
