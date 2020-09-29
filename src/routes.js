a0af35dbd4909f4f15643875c3bcc3fd338c01cb
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-09-28 21:57:33 -0300

    'criadorotdecriarreserva'

--- src/routes.js
+++ src/routes.js
@@ -8,14 +8,17 @@ const upload = multer(uploadConfig)
 import SessionController from './controllers/SessionController'
 import HouseController from './controllers/HouseController'
 import DashboardController from './controllers/DashboardController'
+import ReserveController from './controllers/ReserveController'
 
 routes.post('/sessionuser', SessionController.store)
 
 routes.post('/createhouse', upload.single('thumbnail'), HouseController.store)
 routes.get('/houses', HouseController.index)
 routes.put('/house/:house_id', upload.single('thumbnail'), HouseController.update)
-routes.delete('/house',HouseController.destroy)
+routes.delete('/house', HouseController.destroy)
 
 routes.get('/dashboard', DashboardController.show)
+
+routes.post('/house/:house_id/reserve', ReserveController.store)
 
 export default routes
